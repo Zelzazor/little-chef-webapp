6f54538f814c3c50586b7432eb3b40b00e936d2b
Arturo Hernández <arturoaureliohr@gmail.com>
2023-03-16 00:13:29 -0400

    TASK-0: Add basic Auth0 setup

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,9 +1,10 @@
 import { Route, Routes } from 'react-router-dom';
+import { Home } from './pages/Home/Home';
 
 export const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<>Root</>} />
+      <Route path="/" element={<Home />} />
     </Routes>
   );
 };
