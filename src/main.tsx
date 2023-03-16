9c9ee7a75e37e7c4cb7aeb94dcc42b6e31a6f3a6
Arturo Hernández <arturoaureliohr@gmail.com>
2023-03-15 23:15:17 -0400

    TASK-0: Setup react-query and install axios

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { QueryClientProvider } from 'react-query';
+import { QueryClient } from 'react-query/types/core';
 import App from './App';
+
 import './index.css';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
