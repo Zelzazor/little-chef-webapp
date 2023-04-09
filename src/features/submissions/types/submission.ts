34945ea3c24460713800529bf050800dfaad1adb
Arturo Hernández <arturoaureliohr@gmail.com>
2023-04-08 20:29:27 -0400

    TASK-210: Add foreign entity attributes in Submission entity type

--- src/features/submissions/types/submission.ts
+++ src/features/submissions/types/submission.ts
@@ -1,10 +1,15 @@
+import { User } from '../../user/types/user';
 import { BaseEntity } from '../../utility/types/entity';
+import { SubmissionVote } from './submission-vote';
 
 export type Submission = BaseEntity & {
   imageUrl: string;
   status: SubmissionStatus;
   userId: string;
   recipeId: string;
+  user?: Partial<User>;
+  recipe?: Partial<any>;
+  votes?: Partial<SubmissionVote>[];
 };
 
 export enum SubmissionStatus {
