538ba7acbc6b18b9391118c5eb9f6b60955a10a0
Arturo Hernández <arturoaureliohr@gmail.com>
2023-04-09 17:53:57 -0400

    TASK-210: Add Pagination component

--- src/pages/Submissions/index.tsx
+++ src/pages/Submissions/index.tsx
@@ -1,12 +1,13 @@
+import { Pagination } from 'antd';
 import { FC } from 'react';
 import { SubmissionCard } from '../../features/submissions/components/submission-card/SubmissionCard';
 import { mockedSubmissions } from '../../features/submissions/mocks/submission';
 
 export const Submissions: FC = () => {
   return (
-    <div>
+    <div className="flex flex-col">
       <div className="text-lg font-bold mb-4">Submissions</div>
-      <div className="flex flex-row flex-wrap gap-3 justify-center">
+      <div className="flex flex-row flex-wrap gap-3 justify-center mb-3">
         {mockedSubmissions.map((submission) => {
           return (
             <SubmissionCard
@@ -17,6 +18,13 @@ export const Submissions: FC = () => {
           );
         })}
       </div>
+      <Pagination
+        className="mx-auto"
+        defaultPageSize={12}
+        pageSizeOptions={[12, 24, 48]}
+        defaultCurrent={1}
+        total={1000}
+      />
     </div>
   );
 };
