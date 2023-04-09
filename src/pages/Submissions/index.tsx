e205c7a766030e5482b89f3aa721264ac67739e0
Arturo Hernández <arturoaureliohr@gmail.com>
2023-04-09 15:49:19 -0400

    TASK-210: Add spacing for Submission Cards and flex layout

--- src/pages/Submissions/index.tsx
+++ src/pages/Submissions/index.tsx
@@ -5,9 +5,16 @@ import { mockedSubmissions } from '../../features/submissions/mocks/submission';
 export const Submissions: FC = () => {
   return (
     <div>
-      <div>
+      <div className="text-lg font-bold mb-4">Submissions</div>
+      <div className="flex flex-row flex-wrap">
         {mockedSubmissions.map((submission) => {
-          return <SubmissionCard key={submission.id} submission={submission} />;
+          return (
+            <SubmissionCard
+              key={submission.id}
+              className="mb-3 mr-3"
+              submission={submission}
+            />
+          );
         })}
       </div>
     </div>
