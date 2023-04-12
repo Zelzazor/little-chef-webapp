badc7b50a38588b8eb9e339ba60fb9006a5167b5
Arturo Hernández <a.hernandez@gbh.com.do>
2023-04-12 19:35:22 -0400

    Add dynamic card background color depending on its submission status

--- src/features/submissions/components/submission-card/SubmissionCard.tsx
+++ src/features/submissions/components/submission-card/SubmissionCard.tsx
@@ -1,7 +1,7 @@
 import { Card } from 'antd';
 import { FC } from 'react';
 import NoImage from '../../../utility/assets/images/no-image.png';
-import { Submission } from '../../types/submission';
+import { Submission, SubmissionStatus } from '../../types/submission';
 import { DeleteSubmissionButton } from '../delete-submission-button/DeleteSubmissionButton';
 import { LikeOutlined } from '@ant-design/icons';
 
@@ -17,9 +17,22 @@ export const SubmissionCard: FC<SubmissionCardProps> = ({
   const upvotes = submission.votes?.filter((vote) => vote.isUpvote).length;
   const downvotes = submission.votes?.filter((vote) => !vote.isUpvote).length;
 
+  const getColorClass = () => {
+    switch (submission.status) {
+      case SubmissionStatus.PENDING:
+        return '';
+      case SubmissionStatus.APPROVED:
+        return 'bg-green-100';
+      case SubmissionStatus.REJECTED:
+        return 'bg-red-100';
+    }
+  };
+
   return (
     <Card
-      className={`h-fit ${className ?? ''}`}
+      className={`h-fit ${
+        className ?? ''
+      } ${getColorClass()} border-solid border-[1px] border-slate-200`}
       title={
         <div>
           <div>{submission?.recipe?.name}</div>
